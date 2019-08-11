7e9a3d4606e418e09ed7a4a1210f6806145b3002
mittulmandhan <conanmittul@gmail.com>
2019-08-11 19:49:20 +0530

    completed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,22 @@ import { DepartmentListComponent } from './department-list/department-list.compo
 import { EmployeeListComponent } from './employee-list/employee-list.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { DepartmentDetailComponent } from './department-detail/department-detail.component';
+import { DepartmentOverviewComponent } from './department-overview/department-overview.component';
+import { DepartmentContactComponent } from './department-contact/department-contact.component';
 
 const routes: Routes = [
   // redirecting routes for the default url
   // this statement must be put on the top to do so
   { path: '', redirectTo: '/departments', pathMatch: 'full' },
   { path: 'departments', component: DepartmentListComponent},
-  { path: 'departments/:id', component: DepartmentDetailComponent },
+  {
+    path: 'departments/:id',
+    component: DepartmentDetailComponent,
+    children: [
+      {path: 'overview', component: DepartmentOverviewComponent},
+      {path: 'contact', component: DepartmentContactComponent}
+    ]
+  },
   { path: 'employees', component: EmployeeListComponent },
   // wildcard route
   // this is for displaying the message 'Page Not Found' when navigated page does not exist
@@ -24,4 +33,9 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-export const routingComponents = [DepartmentListComponent, EmployeeListComponent, PageNotFoundComponent, DepartmentDetailComponent];
+export const routingComponents = [DepartmentListComponent,
+                                  EmployeeListComponent,
+                                  PageNotFoundComponent,
+                                  DepartmentDetailComponent,
+                                  DepartmentOverviewComponent,
+                                  DepartmentContactComponent];
--- src/app/department-detail/department-detail.component.ts
+++ src/app/department-detail/department-detail.component.ts
@@ -36,4 +36,12 @@ export class DepartmentDetailComponent implements OnInit {
       this.router.navigate(['/departments', {id: selectedId}]);
     }
 
+    showOverview() {
+      this.router.navigate(['overview'], {relativeTo: this.route});
+    }
+
+    showContact() {
+      this.router.navigate(['contact'], {relativeTo: this.route});
+    }
+
 }
